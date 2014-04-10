5988f1ecc643d00dadf72d4131dba7de541ea5ee
Jan-Michael Brummer <jan.brummer@tabos.org>
2014-04-10 14:31:23 +0000

    * Disable pwd output

--- libroutermanager/plugins/secret/secret.c
+++ libroutermanager/plugins/secret/secret.c
@@ -91,14 +91,12 @@ static gchar *secret_get_password(struct profile *profile, const gchar *name)
 	                  "name", name,
 	                  NULL);
 
-	g_debug("get_password....");
 	if (error != NULL) {
 		/* ... handle the failure here */
 		g_debug("could not get password: %s", error->message);
 		g_error_free(error);
 	} else {
 		/* ... do something now that the password has been stored */
-		g_debug("password: %s", password);
 	}
 
 	return password;
