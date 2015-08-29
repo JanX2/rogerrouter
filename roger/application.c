b76ff7f22381ae0d60f0c81c04106e51c685c310
Jan-Michael Brummer <jan.brummer@tabos.org>
2015-08-29 21:36:05 +0000

    * Journal: Disable debug notification

--- roger/application.c
+++ roger/application.c
@@ -309,7 +309,7 @@ static void app_init(GtkApplication *app)
 		assistant();
 	}
 
-#if 1
+#if 0
 	struct connection *connection = connection_add_call(2, CONNECTION_TYPE_INCOMING, "6173097", "08001888444");
 
 	emit_connection_notify(connection);
