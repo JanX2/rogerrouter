57c5394bdcbbb9d873e4656447dd41358b444fa4
Jan-Michael Brummer <jan.brummer@tabos.org>
2015-11-27 20:59:54 +0000

    * Reverse lookup: Add debug information and change user agent string

--- libroutermanager/plugins/reverselookup/reverselookup.c
+++ libroutermanager/plugins/reverselookup/reverselookup.c
@@ -135,13 +135,13 @@ static gboolean do_reverse_lookup(struct lookup *lookup, gchar *number, gchar **
 #endif
 	SoupURI *uri = soup_uri_new(url);
 	msg = soup_message_new_from_uri(SOUP_METHOD_GET, uri);
-	soup_message_headers_append (msg->request_headers, "User-Agent", "Mozilla/5.0 (Fedora; Linux x86_64) AppleWebKit/602.1 (KHTML, like Gecko) Version/8.0 Safari/602.1 Epiphany/3.17.2");
+	soup_message_headers_append (msg->request_headers, "User-Agent", "Mozilla/5.0 (Fedora; Linux x86_64)");
 
 	soup_session_send_message(soup_session_sync, msg);
 	soup_uri_free(uri);
 	g_free(url);
 	if (msg->status_code != 200) {
-		g_debug("Received status code: %d", msg->status_code);
+		g_debug("Service: %s received status code: %d", lookup->service, msg->status_code);
 		g_object_unref(msg);
 		return FALSE;
 	}
