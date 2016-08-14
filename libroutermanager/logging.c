0bdd9c09a6f6eccf3df3018e039ba366c2ab734a
Jan-Michael Brummer <jan.brummer@tabos.org>
2016-08-14 12:51:38 +0200

    libroutermanager: Only activate new logging structure on glib 2.50.0 and above

--- libroutermanager/logging.c
+++ libroutermanager/logging.c
@@ -68,7 +68,19 @@ static void log_func(const gchar *log_domain, GLogLevelFlags log_level, const gc
 	g_free(time);
 	g_date_time_unref(datetime);
 
-#ifndef G_LOG_USE_STRUCTURED
+#if GLIB_CHECK_VERSION(2,50,0)
+	g_string_append_printf(output, " %s\n", message);
+
+	const GLogField fields[] = {
+		{ "MESSAGE", output->str, -1 }};
+	gchar *fmt_str = g_log_writer_format_fields(log_level, fields, G_N_ELEMENTS (fields), TRUE);
+
+	g_string_free(output, TRUE);
+
+	output = g_string_new("");
+	g_string_append_printf(output, "%s", fmt_str);
+	g_free(fmt_str);
+#else
 	if (log_domain) {
 		g_string_append_printf(output, " %s:", log_domain);
 	}
@@ -91,18 +103,6 @@ static void log_func(const gchar *log_domain, GLogLevelFlags log_level, const gc
 	}
 
 	g_string_append_printf(output, "%s\n", message);
-#else
-	g_string_append_printf(output, " %s\n", message);
-
-	const GLogField fields[] = {
-		{ "MESSAGE", output->str, -1 }};
-	gchar *fmt_str = g_log_writer_format_fields(log_level, fields, G_N_ELEMENTS (fields), TRUE);
-
-	g_string_free(output, TRUE);
-
-	output = g_string_new("");
-	g_string_append_printf(output, "%s", fmt_str);
-	g_free(fmt_str);
 #endif
 
 	g_print("%s", output->str);
