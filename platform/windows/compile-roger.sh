fb7498e170a7c3897bf43b2ca5ffa1e36d677374
Jan-Michael Brummer <jbrummer@buzz-macmini.fritz.box>
2017-11-30 22:17:01 +0100

    win32: Add poppler support for internal fax viewer and update icon

--- platform/windows/compile-roger.sh
+++ platform/windows/compile-roger.sh
@@ -1,6 +1,6 @@
 MXE=$(pwd)/mxe
 export PATH=$MXE/usr/bin:$PATH
-meson ../.. build --cross-file=mingw-cross.txt --prefix=$MXE/i686-w64-mingw32.shared.posix --libdir=lib -Dfax_spooler=false
+meson ../.. build --cross-file=mingw-cross.txt --prefix=$MXE/usr/i686-w64-mingw32.shared.posix --libdir=lib
 cd build
 ninja
 ninja install
